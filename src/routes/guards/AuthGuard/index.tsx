e90cb92d4f0ce5648e9c7f4c5fc02cdd2e6117fe
PavelM <myagkiypavel@gmail.com>
2024-07-14 20:57:52 +0300

    feat: add GuardRoutes

--- src/routes/guards/AuthGuard/index.tsx
+++ src/routes/guards/AuthGuard/index.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
 import { Navigate } from "react-router-dom";
-import { GuestRoutes } from "../../../constants";
+import { GuestRoutes, Tokens } from "../../../constants";
+import Cookies from "js-cookie";
 
 type Props = {
   component: React.ElementType;
-  isAuth: boolean;
 };
 
-const AuthGuard: FC<Props> = ({ component: Component, isAuth }) => {
-  if (!isAuth) {
+const AuthGuard: FC<Props> = ({ component: Component }) => {
+  const isAuthLocal = Boolean(Cookies.get(Tokens.Access));
+
+  if (!isAuthLocal) {
     return <Navigate to={GuestRoutes.RouteSignIn} replace />;
   }
   return <Component />;
